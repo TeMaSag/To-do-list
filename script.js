b8a2b0e620b394341906a6fe4a3bdee1476bcb57
Artem Sagar <sagar.tema61@gmail.com>
2021-10-05 22:31:31 +0300

    Save todos in localStorage

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ let list = document.querySelector(".list");
 let fieldTask = document.querySelector(".field-task");
 let taskList = document.querySelector(".list");
 let deleteCompletedTask = document.querySelector("#deleteCompletedTask");
+let todos;
+
+function toLocalStorage() {
+    todos = list.innerHTML;
+    localStorage.setItem('todos', todos)
+}
 
 //добавление задания
 listForm.onsubmit = function(evt) {
@@ -18,7 +24,7 @@ listForm.onsubmit = function(evt) {
         list.append(newTask);
         fieldTask.value = "";
         fieldTask.placeholder = "Добавьте задание";
-
+        toLocalStorage()
     } else {
         fieldTask.placeholder = "пустое поле";
     }
@@ -28,6 +34,7 @@ listForm.onsubmit = function(evt) {
 taskList.addEventListener("click", function(ev) {
     if (ev.target.tagName === "P") {
         ev.target.classList.toggle("toggle");
+        toLocalStorage()
     }
 }, false);
 
@@ -35,32 +42,39 @@ taskList.addEventListener("click", function(ev) {
 taskList.addEventListener("click", function(ev) {
     if (ev.target.tagName === "SPAN") {
         ev.target.parentNode.parentNode.removeChild(ev.target.parentNode);
+        toLocalStorage()
     }
 }, false);
 
 // удаление завершенных задач
-document.querySelector('#deleteCompletedTask').onclick = function() {
+document.querySelector("#deleteCompletedTask").onclick = function() {
     document.querySelectorAll(".toggle").forEach(e => e.remove());
+    toLocalStorage()
 }
 
 //удаление всез задач
-document.querySelector('#AllDelete').onclick = function() {
+document.querySelector("#AllDelete").onclick = function() {
     document.querySelectorAll("p").forEach(e => e.remove());
+    toLocalStorage()
 }
 
 // Показать неготовые задачт
-document.querySelector('#activeTask').onclick = function() {
+document.querySelector("#activeTask").onclick = function() {
     document.querySelectorAll(".hidden").forEach(e => e.classList.remove("hidden"));
     document.querySelectorAll(".toggle").forEach(e => e.classList.add("hidden"));
 }
 
 // Показать готовые задачи
-document.querySelector('#completedTask').onclick = function() {
+document.querySelector("#completedTask").onclick = function() {
     document.querySelectorAll(".hidden").forEach(e => e.classList.remove("hidden"));
     document.querySelectorAll(".activeTask").forEach(e => e.classList.add("hidden"));
 }
 
 // Показать все задачи
-document.querySelector('#allTask').onclick = function() {
+document.querySelector("#allTask").onclick = function() {
     document.querySelectorAll(".hidden").forEach(e => e.classList.remove("hidden"));
+}
+
+if (localStorage.getItem("todos")) {
+    list.innerHTML = localStorage.getItem("todos")
 }
